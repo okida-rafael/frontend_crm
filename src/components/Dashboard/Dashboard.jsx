f350cac606bac22ab40b8f223b69cf67ef24de05
Rafael <okida.rafael@gmail.com>
2024-06-24 23:29:18 -0300

    Ajuste em Cards

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,7 +2,7 @@ import Card from "../Card/Card";
 
 const Dashboard = () => {
   return (
-    <div className="ml-56 p-4">
+    <div className="ml-56 p-4 flex justify-center items-center">
       <Card />
     </div>
   );
